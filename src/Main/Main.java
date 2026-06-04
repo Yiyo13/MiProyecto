7d86c1c55b9407263fc8636727e1309fe8019c2c
Yiyo <geovanny.obando.olivas@est.una.ac.cr>
2026-06-04 11:08:23 -0600

    agregue codigo

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Login usuarios");
+
+		System.out.println("adios");
 
 	}
 
